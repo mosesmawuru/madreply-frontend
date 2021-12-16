c03bb71cb7f15a97bb60eaa80e3df3d168becb13
stoneheart428 <lovely4you2ai8ni@gmail.com>
2021-12-16 18:23:38 +0900

    update: rebuild the site with new design

--- src/components/letterview/index.tsx
+++ src/components/letterview/index.tsx
@@ -1,8 +1,4 @@
 import {
-  AvatarDiv,
-  UserInfo,
-} from "components/letterlistcard/letterlistcard.styled";
-import {
   Badge,
   HeaderActions,
 } from "components/mylettercard/mylettercard.styled";
@@ -10,8 +6,19 @@ import React from "react";
 import { FiUser } from "react-icons/fi";
 import { GrEdit } from "react-icons/gr";
 import { RiDeleteBinLine } from "react-icons/ri";
+import { BsHandThumbsUp, BsHandThumbsDown } from "react-icons/bs";
+import { BiComment } from "react-icons/bi";
+import { FiFlag } from "react-icons/fi";
+
 import { Div, Text } from "styles/globals.styled";
-import { CommentPart, LetterViewDiv } from "./letterview.styled";
+import {
+  Action,
+  Actions,
+  AvatarDiv,
+  CommentPart,
+  CommentUserInfo,
+  LetterViewDiv,
+} from "./letterview.styled";
 
 const LetterViewCard = () => {
   return (
@@ -27,7 +34,7 @@ const LetterViewCard = () => {
           <br />
           Lorem Ipsum
         </Text>
-        <Text fSize={18} fWeight={700}>
+        <Text fSize={18} fWeight={500}>
           Date: 12-12-2021
         </Text>
       </Div>
@@ -47,20 +54,65 @@ const LetterViewCard = () => {
         <br />
         Lorem Ipsum
       </Text>
+      <Actions>
+        <Action>
+          <Text fColor="#5C5C5C">
+            <BsHandThumbsUp />
+          </Text>
+          <Text ml={5} fSize={15} fColor="#5C5C5C">
+            Recommend
+          </Text>
+        </Action>
+        <Action>
+          <Text fColor="#5C5C5C">
+            <BsHandThumbsDown />
+          </Text>
+          <Text ml={5} fSize={15} fColor="#5C5C5C">
+            Opposite
+          </Text>
+        </Action>
+        <Action>
+          <Text fColor="#5C5C5C">
+            <BiComment />
+          </Text>
+          <Text ml={5} fSize={15} fColor="#5C5C5C">
+            Comment
+          </Text>
+        </Action>
+        <Action>
+          <Text fColor="#5C5C5C">
+            <FiFlag />
+          </Text>
+          <Text ml={5} fSize={15} fColor="#5C5C5C">
+            Report
+          </Text>
+        </Action>
+      </Actions>
       <CommentPart>
-        <UserInfo>
+        <CommentUserInfo>
           <AvatarDiv>
             <FiUser />
           </AvatarDiv>
-          <Div mode="column">
-            <Text fColor="#191D21" fSize={24} fWeight={500}>
+          <div>
+            <Text fSize={24} fWeight={500}>
               Lorem Ipsum
             </Text>
-            <Text fColor="#191D21" fSize={18} mt={10}>
-              Lorem Ipsum
+            <Text fSize={18} mt={5}>
+              @loremip
             </Text>
-          </Div>
-        </UserInfo>
+          </div>
+        </CommentUserInfo>
+        <Div mt={30}>
+          Sesame snaps pudding marshmallow chocolate cake toffee cookie ice
+          cream tiramisu cake. Liquorice croissant jelly lemon drops jelly beans
+          apple pie pudding caramels donut. Cotton candy jujubes danish cookie
+          chocolate bar apple pie. Wafer croissant topping jelly bear claw
+          liquorice. Shortbread soufflé marshmallow icing macaroon macaroon
+          dragée. Cookie halvah bear claw gummi bears croissant tiramisu donut
+          cookie. Marzipan bear claw lollipop ice cream icing candy candy canes.
+          Cake muffin powder jujubes gingerbread.Marzipan bear claw lollipop ice
+          cream icing candy candy canes. Cake muffin powder jujubes gingerbread.
+        </Div>
       </CommentPart>
     </LetterViewDiv>
   );
--- src/components/letterview/letterview.styled.tsx
+++ src/components/letterview/letterview.styled.tsx
@@ -10,3 +10,40 @@ export const CommentPart = styled.div`
   margin-top: 40px;
   border-top: 1px solid #c5c5c5;
 `;
+
+export const CommentUserInfo = styled.div`
+  display: flex;
+  align-items: center;
+
+  margin: 20px 0;
+`;
+
+export const AvatarDiv = styled.div`
+  margin: 0 10px;
+  width: 25px;
+  height: 25px;
+  color: #5c5c5c;
+  margin-right: 20px;
+  svg {
+    width: 25px;
+    height: 25px;
+  }
+`;
+
+export const Actions = styled.div`
+  margin-top: 30px;
+  display: flex;
+  & > *:not(:first-child) {
+    margin-left: 25px;
+  }
+`;
+
+export const Action = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+`;
