1099e45c70d4f11acb1a93ce5773515f7352feba
stoneheart428 <lovely4you2ai8ni@gmail.com>
2022-01-18 00:28:07 +0100

    emailAction test

--- src/actions/emailActions.ts
+++ src/actions/emailActions.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 import { EMAIL_API } from "./api-urls";
 
 export const gmailAuth = async () => {
+  console.log("object");
   const res = await axios
     .get(EMAIL_API.gmailAuth)
     .then((res) => {
+      console.log(res);
       return res.data;
     })
     .catch((err) => {
