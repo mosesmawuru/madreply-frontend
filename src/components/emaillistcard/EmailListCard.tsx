6717624283f1bca7156a7ae0bc88402da6a664b5
stoneheart428 <lovely4you2ai8ni@gmail.com>
2022-01-18 09:07:19 +0100

    asdf

--- src/components/emaillistcard/EmailListCard.tsx
+++ src/components/emaillistcard/EmailListCard.tsx
@@ -4,9 +4,8 @@ import { EmailListCardDiv } from "./emaillistcard.styled";
 const EmailListCard = ({ data }: any) => {
   return (
     <EmailListCardDiv>
-      {/* <p>{new Date(data.internalDate)}</p> */}
-      {/* {data.snippet} */}
-      asd
+      <p>{new Date(data.internalDate)}</p>
+      {data.snippet}
     </EmailListCardDiv>
   );
 };
