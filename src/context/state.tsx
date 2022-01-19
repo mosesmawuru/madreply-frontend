26f70febbf6b564fbe5ba1d7d6d6b56de7660092
stoneheart428 <lovely4you2ai8ni@gmail.com>
2022-01-19 14:14:46 +0100

    a

--- src/context/state.tsx
+++ src/context/state.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import jwt_decode from "jwt-decode";
 import { isPrivateUrl } from "utils/isPrivateUrl";
+import { logout } from "actions/authActions";
 
 const AuthContext = createContext({});
 const LetterContext = createContext({});
@@ -24,20 +25,24 @@ export const AppWrapper = ({ children }: any) => {
     [letterContext]
   );
 
-  const setContext = () => {
+  const setContext = async () => {
     const token = localStorage.user;
 
     if (token) {
       const decoded: any = jwt_decode(String(token));
       if (decoded.exp < Date.now() / 1000) {
-        localStorage.removeItem("user");
-        setAuthContext({
-          ...authContext,
-          isAuthenticated: false,
-          user: "",
-        });
-        if (!isPrivateUrl(router.pathname, false)) {
-          router.push("/getstarted");
+        const res = await logout();
+        if (res.success) {
+          localStorage.removeItem("user");
+          setAuthContext({
+            ...authContext,
+            isAuthenticated: false,
+            user: "",
+          });
+          if (!isPrivateUrl(router.pathname, false)) {
+            router.push("/getstarted");
+          }
+        } else {
         }
       } else {
         setAuthContext({
