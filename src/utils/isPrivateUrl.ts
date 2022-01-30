4b8b394f8d52a89469cc11d9cf13f6ca0dc34dd7
stoneheart428 <lovely4you2ai8ni@gmail.com>
2022-01-30 03:36:39 +0100

    af

--- src/utils/isPrivateUrl.ts
+++ src/utils/isPrivateUrl.ts
@@ -2,11 +2,11 @@ const unauthUrl = ["/signin", "/signup", "/getstarted", "/resetpass"];
 
 export const isPrivateUrl = (path: string, isAuth: boolean) => {
   if (isAuth) {
-    return unauthUrl.filter((item) => item.includes(path)).length > 0
+    return unauthUrl.filter((item) => path.includes(item)).length > 0
       ? false
       : true;
   } else {
-    return unauthUrl.filter((item) => item.includes(path)).length > 0 ||
+    return unauthUrl.filter((item) => path.includes(item)).length > 0 ||
       path === "/"
       ? true
       : false;
